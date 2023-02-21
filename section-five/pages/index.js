c3e8ad58270e85485ee7df60ce9c740d73e1318b
chefnoname <abdicodes94@gmail.com>
2023-02-21 16:06:50 +0000

    pre rendering pages

---
+++ section-five/pages/index.js
@@ -0,0 +1,31 @@
+import fs from "fs/promises";
+import path from "path";
+
+const Home = (props) => {
+  const { products } = props;
+  return (
+    <>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+
+  const jsonData = await fs.readFile(filePath);
+
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: {
+      products: data.products,
+    },
+  };
+}
+
+export default Home;
